5215e6915dc9f70042b1ff0dc01e704a531e926e
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-10-18 18:09:48 -0500

    Initial Commit

---
+++ proj1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  proj1
+//
+//  Created by William Zeng on 10/18/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
