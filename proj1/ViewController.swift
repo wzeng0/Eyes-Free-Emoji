6bcb0379fb96839dd7bc3c88d1acb58513e02f6c
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-10-20 10:16:16 -0500

    All the initial vibrating code

--- proj1/ViewController.swift
+++ proj1/ViewController.swift
@@ -6,14 +6,25 @@
 //
 
 import UIKit
-
+import AudioToolbox
 class ViewController: UIViewController {
+    
+    let seconds = 4.0
+    var val = 0
+    let frequency = [1, 2, 3, 4, 5, 6]
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        override func viewDidLoad() {
+            // do any additional setup after loading the view.
+            super.viewDidLoad()
+            view.backgroundColor = .systemBlue
+            while (val < 6) {
+                let randomName = frequency.randomElement()!
+                for _ in 0...randomName {
+                    AudioServicesPlaySystemSound(SystemSoundID(4095))
+                    Thread.sleep(forTimeInterval: 1)
+                }
+                Thread.sleep(forTimeInterval: 5)
+                val += 1
+            }
+        }
 }
-
