5dee7a8eaf07aa9e69505f35d9f368d786c8bf4f
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-10-23 15:20:30 -0500

    Added comments and deleted some unnecessary functions for now

--- proj1/ViewController.swift
+++ proj1/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import AudioToolbox
 class ViewController: UIViewController {
     
+//    Button for trial 1
     private let button1: UIButton = {
         let button = UIButton()
         button.backgroundColor = .black
@@ -16,45 +17,51 @@ class ViewController: UIViewController {
         button.setTitleColor(.blue, for: .normal)
         return button
     }()
-    private let button2: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .black
-        button.setTitle("Start Set 1", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        return button
-    }()
 
+//    Loading View
     override func viewDidLoad() {
-        // do any additional setup after loading the view.
         super.viewDidLoad()
+        
+//        Background Default Color of Blue (just for now)
         view.backgroundColor = .systemBlue
+        
+//        Adding button for experimental design 1
         view.addSubview(button1)
-        view.addSubview(button2)
         button1.addTarget(self, action: #selector(didTapButton1), for: .touchUpInside)
-        button2.addTarget(self, action: #selector(didTapButton2), for: .touchUpInside)
     }
     
+//    What to do when button 1 is tapped
     @objc func didTapButton1() {
-        set1(numTrials: 6)
-    }
-    @objc func didTapButton2() {
-        set1(numTrials: 6)
+//        Tried changing background color
+        view.backgroundColor = colors.randomElement()
+        
+//          Running the Trail Function
+//        set1(numTrials: 6)
     }
     
+//    Sub Layout view, the view screen is smaller than the main view layout screen
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
+//        Dimensions of button 1
         button1.frame = CGRect(x: 30,
                               y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
                               width: view.frame.size.width - 260,
                               height: 55
         )
-        button2.frame = CGRect(x: 30,
-                              y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
-                              width: view.frame.size.width - 260,
-                              height: 55
-        )
     }
     
+//    Screen Colors for every emotions
+    let colors: [UIColor] = [
+        .systemBlue,
+        .systemRed,
+        .systemGreen,
+        .systemMint,
+        .systemYellow,
+        .systemPurple
+    ]
+    
+//    Trial 1 with standard vibration each representing a different Emotion
     func set1(numTrials: Int) -> Void {
         let seconds = 4.0
         var val = 0
@@ -68,6 +75,7 @@ class ViewController: UIViewController {
             }
             if let index = frequency.firstIndex(of: randomName) {
                 frequency.remove(at: index)
+//                view.backgroundColor = colors[index]
                 print("Index of '\(randomName)' is \(index).")
             }
             Thread.sleep(forTimeInterval: seconds)
