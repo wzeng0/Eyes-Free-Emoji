f7c29bd299d77b490aee460338b831af6335533d
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-10-23 14:44:15 -0500

    Set up a button to run trials

--- proj1/ViewController.swift
+++ proj1/ViewController.swift
@@ -9,26 +9,53 @@ import UIKit
 import AudioToolbox
 class ViewController: UIViewController {
     
-    let seconds = 4.0
-    var val = 0
-    var frequency = [1, 2, 3, 4, 5, 6]
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Start Set 1", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
 
-        override func viewDidLoad() {
-            // do any additional setup after loading the view.
-            super.viewDidLoad()
-            while (val < 6) {
-                let randomName = frequency.randomElement()!
-                for _ in 0...(randomName - 1) {
-                    AudioServicesPlaySystemSound(SystemSoundID(4095))
-                    Thread.sleep(forTimeInterval: 1)
-                }
-                if let index = frequency.firstIndex(of: randomName) {
-                    frequency.remove(at: index)
-                    print("Index of '\(randomName)' is \(index).")
-                }
-                view.backgroundColor = .systemBlue
-                Thread.sleep(forTimeInterval: 5)
-                val += 1
+    override func viewDidLoad() {
+        // do any additional setup after loading the view.
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+        view.addSubview(button)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+//        set1(numTrials: 6)
+    }
+    
+    @objc func didTapButton() {
+        set1(numTrials: 6)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        button.frame = CGRect(x: 30,
+                              y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
+                              width: view.frame.size.width - 60,
+                              height: 55
+        )
+    }
+    
+    func set1(numTrials: Int) -> Void {
+        let seconds = 4.0
+        var val = 0
+        var frequency = [1, 2, 3, 4, 5, 6]
+        
+        while (val < numTrials) {
+            let randomName = frequency.randomElement()!
+            for _ in 0...(randomName - 1) {
+                AudioServicesPlaySystemSound(SystemSoundID(4095))
+                Thread.sleep(forTimeInterval: 1)
             }
+            if let index = frequency.firstIndex(of: randomName) {
+                frequency.remove(at: index)
+                print("Index of '\(randomName)' is \(index).")
+            }
+            Thread.sleep(forTimeInterval: seconds)
+            val += 1
         }
+    }
 }
