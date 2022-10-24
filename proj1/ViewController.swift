16cb847b94a460c8da026020c443f0fc7934274a
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-10-24 12:11:21 -0500

    Done with tapping as response

--- proj1/ViewController.swift
+++ proj1/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import AudioToolbox
 class ViewController: UIViewController {
     
+    var userOutput: [String] = []
+    
 //    Button for trial 1
     private let button1: UIButton = {
         let button = UIButton()
@@ -34,6 +36,50 @@ class ViewController: UIViewController {
 //        Background Default Color of Blue (just for now)
         view.backgroundColor = .systemBlue
         
+        let gestureView = UIView(frame: CGRect(x:0, y:0, width:400, height:500))
+        gestureView.backgroundColor = .red
+        gestureView.center = view.center
+        gestureView.isUserInteractionEnabled = true
+        view.addSubview(gestureView)
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(oneTap(gesture:)))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTouchesRequired = 1
+        let doubleTapGesture = UITapGestureRecognizer(target: self, action: #selector(twoTap(gesture:)))
+        doubleTapGesture.numberOfTapsRequired = 2
+        doubleTapGesture.numberOfTouchesRequired = 1
+        let threeTapGesture = UITapGestureRecognizer(target: self, action: #selector(threeTap(gesture:)))
+        threeTapGesture.numberOfTapsRequired = 3
+        threeTapGesture.numberOfTouchesRequired = 1
+        let fourTapGesture = UITapGestureRecognizer(target: self, action: #selector(fourTap(gesture:)))
+        fourTapGesture.numberOfTapsRequired = 4
+        fourTapGesture.numberOfTouchesRequired = 1
+        let fiveTapGesture = UITapGestureRecognizer(target: self, action: #selector(fiveTap(gesture:)))
+        fiveTapGesture.numberOfTapsRequired = 5
+        fiveTapGesture.numberOfTouchesRequired = 1
+        let sixTapGesture = UITapGestureRecognizer(target: self, action: #selector(sixTap(gesture:)))
+        sixTapGesture.numberOfTapsRequired = 6
+        sixTapGesture.numberOfTouchesRequired = 1
+        
+        
+        
+        gestureView.addGestureRecognizer(tapGesture)
+        gestureView.addGestureRecognizer(doubleTapGesture)
+        gestureView.addGestureRecognizer(threeTapGesture)
+        gestureView.addGestureRecognizer(fourTapGesture)
+        gestureView.addGestureRecognizer(fiveTapGesture)
+        gestureView.addGestureRecognizer(sixTapGesture)
+        
+        func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
+                 shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+           // Don't recognize a single tap until a double-tap fails.
+           if gestureRecognizer == tapGesture &&
+                  otherGestureRecognizer == doubleTapGesture {
+              return true
+           }
+           return false
+        }
+        
 //        Adding button for experimental design 1
         view.addSubview(button1)
         button1.addTarget(self, action: #selector(didTapButton1), for: .touchUpInside)
@@ -41,6 +87,36 @@ class ViewController: UIViewController {
 //        Adding button for experimental design 2
         view.addSubview(button2)
         button2.addTarget(self, action: #selector(didTapButton2), for: .touchUpInside)
+    }
+    
+    @objc func oneTap(gesture: UITapGestureRecognizer) {
+        userOutput.append("happy")
+        print(userOutput)
+    }
+    @objc func twoTap(gesture: UITapGestureRecognizer) {
+        userOutput = userOutput.dropLast(1)
+        userOutput.append("sad")
+        print(userOutput)
+    }
+    @objc func threeTap(gesture: UITapGestureRecognizer) {
+        userOutput = userOutput.dropLast(1)
+        userOutput.append("thumbs up")
+        print(userOutput)
+    }
+    @objc func fourTap(gesture: UITapGestureRecognizer) {
+        userOutput = userOutput.dropLast(1)
+        userOutput.append("thumbs down")
+        print(userOutput)
+    }
+    @objc func fiveTap(gesture: UITapGestureRecognizer) {
+        userOutput = userOutput.dropLast(1)
+        userOutput.append("laugh")
+        print(userOutput)
+    }
+    @objc func sixTap(gesture: UITapGestureRecognizer) {
+        userOutput = userOutput.dropLast(1)
+        userOutput.append("mad")
+        print(userOutput)
     }
     
 //    What to do when button 1 is tapped
