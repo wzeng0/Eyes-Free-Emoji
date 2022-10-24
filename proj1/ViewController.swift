91823941143bdc73ec1f0e833ba1a2ad64dca688
wzeng0 <75603464+wzeng0@users.noreply.github.com>
2022-10-24 12:53:06 -0500

    deleted button 2 functions

--- proj1/ViewController.swift
+++ proj1/ViewController.swift
@@ -11,29 +11,11 @@ class ViewController: UIViewController {
     
     var userOutput: [String] = []
     
-//    Button for trial 1
-    private let button1: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .black
-        button.setTitle("Start Set 1", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        return button
-    }()
-
-//    Button for trial 2
-    private let button2: UIButton = {
-        let button = UIButton()
-        button.backgroundColor = .black
-        button.setTitle("Start Set 2", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
-        return button
-        }()
-    
-//    Loading View
+    //    Loading View
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        Background Default Color of Blue (just for now)
+        //        Background Default Color of Blue (just for now)
         view.backgroundColor = .systemBlue
         
         let gestureView = UIView(frame: CGRect(x:0, y:0, width:400, height:500))
@@ -71,22 +53,18 @@ class ViewController: UIViewController {
         gestureView.addGestureRecognizer(sixTapGesture)
         
         func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
-                 shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-           // Don't recognize a single tap until a double-tap fails.
-           if gestureRecognizer == tapGesture &&
-                  otherGestureRecognizer == doubleTapGesture {
-              return true
-           }
-           return false
+                               shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+            // Don't recognize a single tap until a double-tap fails.
+            if gestureRecognizer == tapGesture &&
+                otherGestureRecognizer == doubleTapGesture {
+                return true
+            }
+            return false
         }
         
-//        Adding button for experimental design 1
+        //        Adding button for experimental design 1
         view.addSubview(button1)
         button1.addTarget(self, action: #selector(didTapButton1), for: .touchUpInside)
-        
-//        Adding button for experimental design 2
-        view.addSubview(button2)
-        button2.addTarget(self, action: #selector(didTapButton2), for: .touchUpInside)
     }
     
     @objc func oneTap(gesture: UITapGestureRecognizer) {
@@ -119,53 +97,33 @@ class ViewController: UIViewController {
         print(userOutput)
     }
     
-//    What to do when button 1 is tapped
+    //    Button for trial 1
+    private let button1: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .black
+        button.setTitle("Start Set 1", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        return button
+    }()
+    
+    //    What to do when button 1 is tapped
     @objc func didTapButton1() {
-//        Tried changing background color
-        view.backgroundColor = colors.randomElement()
-//          Running the Trail Function
         set1(numTrials: 6)
     }
     
-//    What to do when button 2 is tapped
-    @objc func didTapButton2() {
-//        Tried changing background color
-        view.backgroundColor = colors.randomElement()
-//          Running the Trail Function
-        set2(numTrials: 6)
-    }
-    
-    
-//    Sub Layout view, the view screen is smaller than the main view layout screen
+    //    Sub Layout view, the view screen is smaller than the main view layout screen
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-//        Dimensions of button 1
+        //        Dimensions of button 1
         button1.frame = CGRect(x: 30,
-                              y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
-                              width: view.frame.size.width - 260,
-                              height: 55
+                               y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
+                               width: view.frame.size.width - 260,
+                               height: 55
         )
-
-//        Dimensions of button 2
-        button2.frame = CGRect(x: 200,
-                                y: view.frame.size.height - 150 - view.safeAreaInsets.bottom,
-                                width: view.frame.size.width - 260,
-                                height: 55
-                )
     }
     
-//    Screen Colors for every emotions
-    let colors: [UIColor] = [
-        .systemBlue,
-        .systemRed,
-        .systemGreen,
-        .systemMint,
-        .systemYellow,
-        .systemPurple
-    ]
-    
-//    Trial 1 with standard vibration each representing a different Emotion
+    //    Trial 1 with standard vibration each representing a different Emotion
     func set1(numTrials: Int) -> Void {
         let seconds = 4.0
         var val = 0
@@ -179,34 +137,11 @@ class ViewController: UIViewController {
             }
             if let index = frequency.firstIndex(of: randomName) {
                 frequency.remove(at: index)
-//                view.backgroundColor = colors[index]
+                //                view.backgroundColor = colors[index]
                 print("Index of '\(randomName)' is \(index).")
             }
             Thread.sleep(forTimeInterval: seconds)
             val += 1
         }
     }
-    
-//    Trial 2 with different vibrations each representing a different Emotion
-        func set2(numTrials: Int) -> Void {
-            let seconds = 4.0
-            var val = 0
-            var frequency = [4095, 1304, 1321, 1519, 1350, 1521]
-            
-            // 4095 = normal, 1521 = 3 little booms, 1321 = two
-            // need to change 1350, also 1519 is very close to 1521
-
-            while (val < numTrials) {
-                let randomName = frequency.randomElement()!
-                AudioServicesPlaySystemSound(SystemSoundID(randomName))
-                
-                if let index = frequency.firstIndex(of: randomName) {
-                    frequency.remove(at: index)
-//                view.backgroundColor = colors[index]
-                    print("Index of '\(randomName)' is \(index).")
-                }
-                Thread.sleep(forTimeInterval: seconds)
-                val += 1
-            }
-        }
 }
